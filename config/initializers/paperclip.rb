d857c9de2fb018b8adedd1f287c810be508e1dbc
Timothy Sherratt <tim@mitoo.co>
2016-08-30 16:19:18 -0700

    add prod fog config

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,30 +1,30 @@
 RACKSPACE_CONFIG = {
   'production' => {
-    path: '',
+    path: ENV['S3_BUCKET'],
     storage: :fog,
     fog_credentials: {
       provider: 'AWS',
-      aws_access_key_id: 'AKIAJKUC2CHYCGLXQK2Q',
-      aws_secret_access_key: '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',
+      aws_access_key_id: ENV['AWS_ACCESS_KEY'],
+      aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
       persistent: false,
-      region: 'eu-west-1'
+      region: ENV['AWS_REGION'],
     },
-    fog_directory: "bacon-uploads-prod",
+    fog_directory: ENV['S3_BUCKET'],
     fog_public: true,
-    fog_host: 'http://bacon-uploads-prod.s3.amazonaws.com',
+    fog_host: "http://s3-eu-west-1.amazonaws.com",
   },
   'staging' => {
     path: '',
     storage: :fog,
     fog_credentials: {
       provider: 'AWS',
-      aws_access_key_id: 'AKIAJKUC2CHYCGLXQK2Q',
-      aws_secret_access_key: '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',
+      aws_access_key_id: '',
+      aws_secret_access_key: '',
       persistent: false
     },
-    fog_directory: "bacon-uploads-staging", # NOTE: DOES NOT EXIST (though neither does staging)
+    fog_directory: ''
     fog_public: true,
-    fog_host: 'http://bacon-uploads-prod.s3.amazonaws.com'
+    fog_host: ''
   },
   'development' => {
     path: ENV['S3_BUCKET'],
