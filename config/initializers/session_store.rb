fb3ef1a6cc9185f47f2176ce12f70cfed07c3286
Timothy Sherratt <tim@mitoo.co>
2016-11-02 12:44:35 -0700

    update session cookie to work across domains

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,4 +1,4 @@
 # Be sure to restart your server when you modify this file.
 
-BluefieldsRails::Application.config.session_store :cookie_store, key: '_bluefields-rails_session'
+BluefieldsRails::Application.config.session_store :cookie_store, key: '_bluefields-rails_session', domain: ".#{$ROOT_DOMAIN}"
 # BluefieldsRails::Application.config.session_store :redis_store, servers: "#{ENV['REDIS_STORE_URL']}/1/sessions", expire_in: 259200, domain: ".#{$ROOT_DOMAIN}"
