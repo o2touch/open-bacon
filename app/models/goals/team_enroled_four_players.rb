3a2479b728f172c6ae13c8b4411aadd7d8948dba
Timothy Sherratt <tim@mitoo.co>
2016-11-14 16:18:25 -0800

    comment goal ting, as doing a bad thing

--- app/models/goals/team_enroled_four_players.rb
+++ app/models/goals/team_enroled_four_players.rb
@@ -5,10 +5,11 @@ class TeamEnroledFourPlayers < GoalCheckListItem
   end
 
   def complete?
-    players_id = @team.players(true).map {|x| x.id}
-    demo_players_id = @team.demo_players.map {|x| x.id}
+    # players_id = @team.players(true).map {|x| x.id}
+    # demo_players_id = @team.demo_players.map {|x| x.id}
 
-    (players_id - demo_players_id - [@team.founder.id]).count >= 4
+    # (players_id - demo_players_id - [@team.founder.id]).count >= 4
+    true
   end
 
   def notify
